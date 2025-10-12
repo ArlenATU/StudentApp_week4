7079ec8e192d00d0cffa6d2af2eb93fcd9b87068
Denys Kapustianskyi <denyskapustianskyi@icloud.com>
2025-10-12 11:57:47 +0100

    I've created a new class called StudentApp

---
+++ src/main/java/ie/atu/week4/StudentApp.java
@@ -0,0 +1,18 @@
+package ie.atu.week4;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many student do you want to enter? ");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        int i = 0;
+        while (i < count) {
+            System.out.println();
+        }
+    }
+
+}
