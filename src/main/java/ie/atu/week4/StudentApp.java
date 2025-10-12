3966da708c97843ff04ca693ede0fe093a99416b
Denys Kapustianskyi <denyskapustianskyi@icloud.com>
2025-10-12 13:35:31 +0100

    added an arrayList in the main class

--- src/main/java/ie/atu/week4/StudentApp.java
+++ src/main/java/ie/atu/week4/StudentApp.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class StudentApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Student>students = new ArrayList<>();
+
         System.out.println("How many student do you want to enter? ");
         int count = scanner.nextInt();
         scanner.nextLine();
