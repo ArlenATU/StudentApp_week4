6e1f71654be67dea11577cb8d22ff2aaca8c96a2
Denys Kapustianskyi <denyskapustianskyi@icloud.com>
2025-10-12 15:59:25 +0100

    fixed bugs, updated code

--- src/main/java/ie/atu/week4/StudentApp.java
+++ src/main/java/ie/atu/week4/StudentApp.java
@@ -1,24 +1,38 @@
 package ie.atu.week4;
-
+import java.util.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Student st = new Student();
         ArrayList<Student>students = new ArrayList<>();
+        Set<String> emails = new HashSet<>();
+
 
         System.out.print("How many student do you want to enter?: ");
         int count = scanner.nextInt();
         scanner.nextLine();
 
         int i = 0;
+
         while (i < count) {
+            boolean unique = false;
             System.out.print("Enter your name: ");
             String name = scanner.nextLine();
-            System.out.print("Enter your email: ");
-            String email = scanner.nextLine();
+
+            String email;
+            do {
+                System.out.print("Enter your email: ");
+                email = scanner.nextLine().toLowerCase();
+
+                if (emails.contains(email)) {
+                    System.out.println("Email " + email + " is already in use. Please enter a different one.");
+                }
+            } while (emails.contains(email));
+
+            emails.add(email);
+
             System.out.print("Enter the name of your course: ");
             String course = scanner.nextLine();
             students.add(new Student(name, email, course));
